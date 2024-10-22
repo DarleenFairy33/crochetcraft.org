68c61b3d8d7df33cd46196af492cf5a07cf53c95
Darleen <darleenfairy33@myyahoo.com>
2024-10-22 16:51:08 -0400

    MAIN UPLOADS

---
+++ signup.js
@@ -0,0 +1,18 @@
+function setAction(form) {
+  form.action = "index.html";
+  alert(form.action);
+  return false;
+}
+
+function registerUser() {
+  document.user_form.action = "index.html";
+  alert(document.user_form.action);
+  return false;
+}
+
+function loginUser() {
+  document.user_form.action = "index.html";
+  alert(document.user_form.action);
+  return false;
+}
+
